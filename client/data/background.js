1250fe2edf9c9c0af9c6fde6534b03ef991b2519
Jefffrey <tpigarelli@gmail.com>
2016-01-29 13:19:05 +0100

    Removed broken functionality
    The functionality for seeing who starred a message is broken
    undefinitely. That section has been removed and can be brought back
    later on (but I doubt they'll allow that).

--- client/data/background.js
+++ client/data/background.js
@@ -20,58 +20,6 @@ function pollForDomNodes(query, init) {
     window.setInterval(poll, 1000);
 }
 
-function starIdentifier() {
-    function getStars(messageID, callback) {
-        return $.ajax({
-            type: "GET",
-            url: host + "/stars",
-            data: {message_id: messageID},
-            success: function(value){callback(JSON.parse(value));},
-        });
-    }
-
-    function getStarsFromDOM(star, box) {
-        box.html(''); // Clear the box out
-        
-        var myMessageID = parseID(star.parents('li, .message').attr('id'));
-        return getStars(myMessageID, function(stars){
-            box.append('<h4> Starred by: </h4>');
-            var innerList = $('<ul>');
-            var names = stars.map(function(star){
-                return star.user_name;
-            });
-            for (var name of names) {
-                var listItem = $('<li>');
-                listItem.text(name);
-                innerList.append(listItem);
-            }
-            box.append(innerList);
-        });
-    }
-
-    function setUpStar(star) {
-        var box = $('<div class="blah"/>');
-        star.append(box);
-        var currentRequest = getStarsFromDOM(star, box);
-        star.parent().mouseenter(function(){
-            currentRequest.abort();
-            currentRequest = getStarsFromDOM(star, box);
-            box.show();
-        });
-        star.parent().mouseleave(function(){
-            box.hide();
-        });
-    }
-
-    function lookForMoreStars() {
-        return Array.from($("span.stars:visible")).filter(function(newStar){
-            return $(newStar).parent("span.meta").length == 0;
-        });
-    }
-
-    pollForDomNodes(lookForMoreStars, setUpStar);
-}
-
 function removedMessageShower() {
     function getMessage(messageID, callback) {
         return $.ajax({
@@ -100,6 +48,5 @@ function removedMessageShower() {
 }
 
 $(function(){
-    starIdentifier();
     removedMessageShower();
 });
